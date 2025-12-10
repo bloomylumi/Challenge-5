da4fb04b11228ecf72ceae039a69f68bf702ea9f
SuperMario202025 <lenartowiczmario@gmail.com>
2025-12-10 10:08:26 +0100

    Added a ton of shit like JS file, and stuff to the home html. Trying to get dropdown to work, still trying

---
+++ html files/Functions.js
@@ -0,0 +1,5 @@
+
+
+const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
+const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl))
+
